5eab93bdefe2d28eb04a2bc21bbf336f0336a677
hemingerg1 <113390240+hemingerg1@users.noreply.github.com>
2024-02-02 14:55:53 -0500

    updates

--- HMI/include/secrets.h
+++
@@ -1,9 +0,0 @@
-// WIFI settings
-#define WIFI_SSID "SSID"
-#define WIFI_PASSWORD "PASSWORD"
-
-// InfluxDB settings
-#define INFLUXDB_URL "server-url"
-#define INFLUXDB_TOKEN "server token"
-#define INFLUXDB_ORG "org name/id"
-#define INFLUXDB_BUCKET "bucket name"
