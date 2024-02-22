72c503ae599a851cdc00321c9105a5c4351ab6aa
hemingerg1 <gmheminger1@gmail.com>
2024-02-22 15:29:00 -0500

    add heaterPLC

---
+++ HeaterPLC/include/secrets_example.h
@@ -0,0 +1,9 @@
+// WIFI settings
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+// MQTT settings
+#define MQTT_SERVER "broker ip"
+#define MQTT_PORT 1883
+#define MQTT_USER "user"
+#define MQTT_PASSWORD "pswd"
---
+++ HeaterPLC/src/main.cpp
@@ -0,0 +1,127 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <MQTT.h>
+#include <secrets.h>
+
+WiFiClient wifiClient;
+MQTTClient mqtt;
+
+#define HEATERPIN 2
+#define FANPIN 12
+
+unsigned long kaTimer = 0;
+unsigned int kaCount = 0;
+bool heatOn = false;
+short int ii = 0;
+
+bool heaterStatus = false;
+bool fanStatus = false;
+
+#include "connectionFunc.hpp"
+void turnHeaterOn();
+void turnHeaterOff();
+void getStatus();
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(F("****** Initializing PLC ******"));
+
+  pinMode(HEATERPIN, OUTPUT);
+  pinMode(FANPIN, OUTPUT);
+  digitalWrite(HEATERPIN, LOW);
+  digitalWrite(FANPIN, LOW);
+
+  wificon();
+  mqttCon();
+}
+
+void loop()
+{
+  ii++;
+
+  // check if MQTT is still connected
+  if (!mqtt.connected())
+  {
+    mqttCon();
+  }
+  // send and receive MQTT messages
+  mqtt.loop();
+
+  // keep alive timer has expired without refresh so turn heater off
+  if (heatOn and (millis() - kaTimer > 75000))
+  {
+    turnHeaterOff();
+  }
+  // mqtt requested heater off
+  else if (heatOn and kaCount == 0)
+  {
+    turnHeaterOff();
+  }
+  // mqtt requested heater on
+  else if (!heatOn and kaCount > 0)
+  {
+    turnHeaterOn();
+  }
+
+  if (ii >= 20)
+  {
+    getStatus();
+    ii = 0;
+  }
+  delay(250);
+}
+
+void turnHeaterOn()
+{
+  Serial.println("Turning heater on");
+  digitalWrite(HEATERPIN, HIGH);
+  delay(1000);
+  digitalWrite(FANPIN, HIGH);
+  heatOn = true;
+}
+
+void turnHeaterOff()
+{
+  Serial.println("Turning heater off");
+  // turn off heating element off
+  digitalWrite(HEATERPIN, LOW);
+  // leave fan on too cool heater down befor turning off
+  delay(30000);
+  digitalWrite(FANPIN, LOW);
+  heatOn = false;
+}
+
+void getStatus()
+{
+  if (digitalRead(HEATERPIN) == HIGH)
+  {
+    heaterStatus = true;
+  }
+  else
+  {
+    heaterStatus = false;
+  }
+
+  if (digitalRead(FANPIN) == HIGH)
+  {
+    fanStatus = true;
+  }
+  else
+  {
+    fanStatus = false;
+  }
+
+  if (heaterStatus and fanStatus and heatOn)
+  {
+    mqtt.publish("Garage/Mech/VentFan/Status", "ON");
+  }
+  else if (fanStatus)
+  {
+    mqtt.publish("Garage/Mech/VentFan/Status", "FAN ON");
+  }
+  else
+  {
+    mqtt.publish("Garage/Mech/VentFan/Status", "OFF");
+  }
+}
